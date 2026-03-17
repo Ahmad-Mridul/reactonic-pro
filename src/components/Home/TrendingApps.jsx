69d06b0bb9946405aa2b6b2d8f89ab7bad6d41be
Ahmad-Mridul <mridulahmed262@gmail.com>
2026-03-17 14:05:57 +0600

    data added and start working trending app section

---
+++ src/components/Home/TrendingApps.jsx
@@ -0,0 +1,13 @@
+const TrendingApps = () => {
+    return (
+        <div className="bg-white text-center text-black p-10 space-y-5">
+            <p className="text-4xl font-bold">Trending Apps</p>
+            <p className="text-gray-500">Explore All Trending Apps on the Market developed by us</p>
+            <div className="">
+                
+            </div>
+        </div>
+    );
+};
+
+export default TrendingApps;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/Home/Hero";
 import Info from "../components/Home/Info";
+import TrendingApps from "../components/Home/TrendingApps";
 
 const Home = () => {
     return (
         <div>
             <Hero/>
             <Info/>
+            <TrendingApps/>
         </div>
     );
 };
